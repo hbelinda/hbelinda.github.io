2d3bd1c944402365c4802dcd1d59e6d08a28c33e
Huang <huan7854@STTHOMAS.EDU>
2018-02-12 10:44:23 -0600

    guess a number

---
+++ guessANumber.js
@@ -0,0 +1,18 @@
+/*
+	Javascript for Guess A Number webpage
+*/
+
+
+
+
+window.onload = function()
+{
+	var answer = math.floor(math.random() *101);
+	alert(answer);
+}
+
+function colorBar(guess)
+{
+	
+	
+}
--- thug.js
+++ thug.js
@@ -3,5 +3,23 @@
 */
 
 
-alert("This Book is AMAZING");
-console.log("talking about THUG");
+
+
+window.onload = function()
+{
+	alert("This Book is AMAZING");
+	console.log("lets talk about THUG");
+	var movieInfo = document.getElementById("movieInfo");
+	movieInfo.onmouseover = wordWave(movieInfo.innerHTML);
+	
+}
+
+function wordWave(words)
+{
+	for(var i = 0; i<words.length; i++)
+	{
+		var randomColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+		words.charAt(i).fontcolor(randomColor);
+	}
+	
+}
