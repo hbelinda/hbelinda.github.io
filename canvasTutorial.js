cee6b43bbd117e7b27f6d844c84b57c13f569c40
Huang <huan7854@stthomas.edu>
2018-02-28 10:38:17 -0600

    to do

--- canvasTutorial.js
+++ canvasTutorial.js
@@ -12,6 +12,17 @@ function draw()
 {
 	var canvas = document.getElementById("theCanvas");
 	var context = canvas.getContext("2d");
+	
+	var ratio = window.devicePixelRatio;
+	var old_width = canvas.width;
+	var old_height = canvas.height;
+	canvas.width = old_width * ratio;
+	canvas.height = old_height * ratio;
+	canvas.style.width = old_width + "px";
+	canvas.style.height = old_height + "px";
+	context.scale(ratio, ratio);
+	
+	
 	context.fillStyle = "#2f3520";
 	context.fillRect(10,10,50,50);
 	
@@ -26,7 +37,8 @@ function draw()
 	context.lineTo(100,80);
 	context.lineTo(100,35);
 	context.fill();
-	
+
+/*	
 	var req = new XMLHttpRequest();
 	req.onreadystatechange = function(){
 		if(req.readyState ==4 && req.status == 200){
@@ -38,5 +50,8 @@ function draw()
 	req.open("GET", "http://freegeoip.net/json/111.118.10.64", true);
 //	req.open("GET", "http://swapi.co/api/people/1", true);
 	req.send();
+*/
+
+
 	
 }
